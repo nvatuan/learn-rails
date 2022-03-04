e9bba97c9f4eb9d197512afd6a2ff7d19771d774
nvat <24392632+nvatuan@users.noreply.github.com>
2022-03-04 17:35:45 +0700

    Chapter 7 (#4)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,32 @@
 class UsersController < ApplicationController
-  def new; end
+  def index; end
+
+  def show
+    @user = User.find params[:id]
+  rescue ActiveRecord::RecordNotFound
+    flash[:danger] = t ".not_found"
+    redirect_to root_path
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t ".create_succeeded"
+      redirect_to @user
+    else
+      flash[:danger] = t ".create_failed"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
